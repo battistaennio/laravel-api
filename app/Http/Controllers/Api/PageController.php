4279df7ddf14b6739bced88c949f3a8ba7c36cf6
battistaennio <ennio.battista99@gmail.com>
2024-09-30 16:04:08 +0200

    feat: add all projects api with type and technologies

---
+++ app/Http/Controllers/Api/PageController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+
+
+// API da realizzare:
+// - elenco di tutti i progetti con associato il tipo e le tecnologie (con o senza paginazione)
+// - elenco di tutte le tecnologie
+// - elenco di tutti i tipi
+// BONUS
+// - dettaglio del singolo post con tipo e tecnologie associate dato lo slug. Aggiungere la gestione del progetto mancante e dell’immagine mancante
+// - elenco dei progetti in base al tipo
+// - elenco dei progetti in base alle tecnologie
+
+
+class PageController extends Controller
+{
+    public function allProjects()
+    {
+
+        $projects = Project::orderBy('id', 'desc')->with('type', 'technologies')->get();
+
+        if ($projects) {
+            $success = true;
+        } else {
+            $success = false;
+        }
+
+        return response()->json(compact('success', 'projects'));
+    }
+}
